75c8e9ca0d9f00bd7c386d5f188656bd77976d5e
shri-itsMe <shri.nallanc@gmail.com>
2026-06-01 12:52:39 -0400

    init with sfml

---
+++ Cell.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Cell.h"
+
+/*
+CELL OBJs
+Each is an int with states: 0 for unvisited, 1 for visited
+also has some way to track walls
+*/
+
+
+Cell::Cell(int rows, int cols)
+{
+
+
+}
+
+Cell::~Cell()
+{
+}
---
+++ Cell.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#pragma once
+
+class Cell
+{
+private:
+    int i;
+    int j;
+public:
+    Cell(int rows, int cols);
+    ~Cell();
+};
+
